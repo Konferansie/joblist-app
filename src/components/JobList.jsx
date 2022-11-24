779a117dd01f08b58cd38c5a4f0debce9779265e
atavaliev <atavaliev@sibers.com>
2022-11-24 14:49:39 +0600

    add project

---
+++ src/components/JobList.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import {JobPosition} from './JobPosition';
+import {useSelector, useDispatch} from "react-redux";
+import {selectAllPositions, selectVisiblePositions} from "../store/positions/position-selectors";
+import {addFilter} from "../store/filters/filter-actions";
+import {selectFilters} from "../store/filters/filter-selector";
+
+const JobList = () => {
+    const dispatch = useDispatch();
+    const currentFilters = useSelector(selectFilters)
+    const positions = useSelector((state) => selectVisiblePositions(state, currentFilters));
+
+    const handleAddFilter = (filter) => {
+        dispatch(addFilter(filter))
+    }
+
+    return (
+        <div className='job-list'>
+            {positions.map(item => (
+                <JobPosition
+                    key={item.id}
+                    {...item}
+                    handleAddFilter={handleAddFilter}
+                />
+            ))}
+        </div>
+    )
+}
+
+export {JobList};
